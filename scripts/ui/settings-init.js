c564f5d1342f584cd0ab2aa60511a92c286d22af
Leonardo Ascione <112330494+rockfactory@users.noreply.github.com>
2025-03-31 00:44:42 +0200

    Move settings into new "Add-ons" category

--- scripts/ui/settings-init.js
+++ scripts/ui/settings-init.js
@@ -1,9 +1,15 @@
 import { Options, OptionType } from '/core/ui/options/model-options.js';
-import { CategoryType } from '/core/ui/options/options-helpers.js';
+import { CategoryType, CategoryData } from '/core/ui/options/options-helpers.js';
 import { PolicyYieldsSettings } from '../core/settings.js';
 
 // We add a dependency on the Options module to ensure default options are loaded before we add our own
 import '/core/ui/options/options.js';
+
+CategoryType["Mods"] = "mods";
+CategoryData[CategoryType["Mods"]] = {
+    title: "LOC_UI_CONTENT_MGR_SUBTITLE",
+    description: "LOC_UI_CONTENT_MGR_SUBTITLE_DESCRIPTION",
+};
 
 const onOptionColorfulInit = (optionInfo) => {
     optionInfo.currentValue = PolicyYieldsSettings.IsColorful;
@@ -14,7 +20,7 @@ const onOptionColorfulUpdate = (optionInfo, value) => {
 
 Options.addInitCallback(() => {
     Options.addOption({ 
-        category: CategoryType.Game, 
+        category: CategoryType["Mods"], 
         // @ts-ignore
         group: 'lf_yields', 
         type: OptionType.Checkbox, 
