d880e7fb48d775f8a70e99c6e676efc79b120204
B. Szonye <bszonye@gmail.com>
2025-08-27 19:29:08 -0700

    compatibility hotfix for Update 1.2.4

--- scripts/ui/settings-init.js
+++ scripts/ui/settings-init.js
@@ -1,9 +1,8 @@
-import { Options, OptionType } from '/core/ui/options/model-options.js';
-import { CategoryType, CategoryData } from '/core/ui/options/options-helpers.js';
+import { O as Options, a as OptionType, d as CategoryData, C as CategoryType } from '/core/ui/options/editors/index.chunk.js';
 import { PolicyYieldsSettings } from '../core/settings.js';
 
 // We add a dependency on the Options module to ensure default options are loaded before we add our own
-import '/core/ui/options/options.js';
+import '/core/ui/options/screen-options.js';
 
 CategoryType["Mods"] = "mods";
 CategoryData[CategoryType["Mods"]] = {
