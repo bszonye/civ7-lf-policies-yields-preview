64bf62c1e29ee8521a0be2f2eb1a75035b83d21b
B. Szonye <bszonye@gmail.com>
2025-11-04 11:57:35 -0800

    Options workaround for Update 1.3.0

--- scripts/ui/settings-init.js
+++ scripts/ui/settings-init.js
@@ -17,6 +17,15 @@ const onOptionColorfulUpdate = (optionInfo, value) => {
     PolicyYieldsSettings.IsColorful = value;
 }
 
+// fix Options initialization
+Options.addInitCallback = function(callback) {
+    if (this.optionsReInitCallbacks.length && !this.optionsInitCallbacks.length) {
+        throw new Error("Options already initialized, cannot add init callback");
+    }
+    this.optionsInitCallbacks.push(callback);
+    this.optionsReInitCallbacks.push(callback);
+}
+
 Options.addInitCallback(() => {
     Options.addOption({ 
         category: CategoryType["Mods"], 
